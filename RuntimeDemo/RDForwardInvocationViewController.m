f2fb025ddb71d92fa8fba68de91bb051c2257bcf
Jeremy Conkin <jeremy@jeremyconkin.com>
2014-05-29 02:41:27 -0500

    Forward invocation example implemented

--- RuntimeDemo/RDForwardInvocationViewController.m
+++ RuntimeDemo/RDForwardInvocationViewController.m
@@ -14,7 +14,30 @@
     
     [super viewDidAppear:animated];
 
-    [self.randomColorView setRandomBackgroundColor];
+    [self performSelector:@selector(setRandomBackgroundColor) withObject:self];
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+    
+    // If the method signature is not found on this class, find it on the
+    // random color view
+    NSMethodSignature* signature = [super methodSignatureForSelector:selector];
+    if (!signature) {
+        signature = [self.randomColorView methodSignatureForSelector:selector];
+    }
+    return signature;
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation
+{
+    // This class does not respond to the selector sent. Try the random color
+    // view instead.
+    if ([self.randomColorView respondsToSelector:[anInvocation selector]]) {
+        [anInvocation invokeWithTarget:self.randomColorView];
+    }
+    else {
+        [super forwardInvocation:anInvocation];
+    }
 }
 
 @end
